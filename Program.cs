e5b92d194738397944d89bd8f7c7505bc55f735d
northdigital <10321991+northdigital@users.noreply.github.com>
2020-10-12 22:13:02 +0300

    commit

--- Program.cs
+++ Program.cs
@@ -6,6 +6,11 @@ namespace AsyncAwait
 {
   class Program
   {
+    // whatever is a Task is awaitable
+    // whatever is async it is waiting for something, if it is also a Task it is also awaitable
+    // we cannot await async void functions and these run syncronously
+    // public static async Task f5() is awaitable
+    // public static async void f5() is not
 
     #region async Task<int> must return an int value
 
@@ -36,17 +41,10 @@ namespace AsyncAwait
     
     public static Task f4()
     {
-      var task = Task.Run(() =>
+      return Task.Run(() =>
       {
         Thread.Sleep(3000);  
       });
-
-      task.Wait();
-
-      return task;
-
-      // OR
-      //return Task.CompletedTask;
     }
 
     #endregion
@@ -60,7 +58,18 @@ namespace AsyncAwait
       if (a == "b")
         return;
 
-      await Task.Delay(11);
+      await Task.Delay(3000);
+    }
+
+    public static async void f6()
+    {
+      string a = string.Empty;
+
+      if (a == "b")
+        return;
+
+      await Task.Delay(3000); // εδώ παίζει το await αλλά η f6 δεν είναι awaitable
+      Console.WriteLine("I am here!");
     }
 
     #endregion
@@ -71,8 +80,10 @@ namespace AsyncAwait
       var i2 = await f2();
       await f4();
       await f5();
+      f6();
 
       Console.WriteLine($"-> {i1} {i2}");
+      Console.ReadLine(); // χωρίς αυτό το λεκτικό "I am here!" δεν θα εμφανιστεί
     }
   }
 }
